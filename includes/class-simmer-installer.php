391034410ac2abaae59bb5253ad045210c25dd28
Chase Wiseman <contact@chasewiseman.com>
2015-04-29 09:28:33 -0700

    Enhance the install/upgrade process

--- includes/class-simmer-installer.php
+++ includes/class-simmer-installer.php
@@ -47,194 +47,10 @@ final class Simmer_Installer {
 	}
 	
 	/**
-	 * Create the custom database tables.
+	 * Uninstall Simmer. Sad face.
 	 *
-	 * @since  1.3.0
-	 * @access private
-	 *
-	 * @global $wpdb The WordPress database object.
+	 * @since 1.3.0
 	 */
-	private static function create_db_tables() {
-		
-		global $wpdb;
-		
-		$items_table_name     = $wpdb->prefix . 'simmer_recipe_items';
-		$item_meta_table_name = $wpdb->prefix . 'simmer_recipe_itemmeta';
-		
-		$query = '';
-		
-		$charset_collate = $wpdb->get_charset_collate();
-		
-		if ( $items_table_name != $wpdb->get_var( "SHOW TABLES LIKE '$items_table_name'" ) ) {
-			
-			// The recipe items table.
-			$query .= "CREATE TABLE $items_table_name (
-				recipe_item_id bigint(20) NOT NULL auto_increment,
-				recipe_item_type varchar(200) NOT NULL DEFAULT '',
-				recipe_id bigint(20) NOT NULL,
-				recipe_item_order int(11) NOT NULL DEFAULT '0',
-				PRIMARY KEY  (recipe_item_id),
-				KEY recipe_id (recipe_id)
-				) $charset_collate;";
-		}
-		
-		if ( $item_meta_table_name != $wpdb->get_var( "SHOW TABLES LIKE '$item_meta_table_name'" ) ) {
-			
-			// The recipe item meta table.
-			$query .= "CREATE TABLE $item_meta_table_name (
-				meta_id bigint(20) NOT NULL auto_increment,
-				recipe_item_id bigint(20) NOT NULL,
-				meta_key varchar(255) NULL,
-				meta_value longtext NULL,
-				PRIMARY KEY  (meta_id),
-				KEY recipe_item_id (recipe_item_id),
-				KEY meta_key (meta_key)
-				) $charset_collate;";
-		}
-		
-		if ( '' != $query ) {
-			
-			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-			
-			dbDelta( $query );
-		}
-	}
-	
-	private static function upgrade() {
-		
-		global $wpdb;
-		
-		// Get all recipe IDs.
-		$recipe_ids = get_posts( array(
-			'post_type'   => 'recipe',
-			'post_status' => 'any',
-			'numberposts' => -1,
-			'fields'      => 'ids',
-		) );
-		
-		if ( $recipe_ids ) {
-			
-			$items_api = new Simmer_Recipe_Items;
-			
-			foreach ( $recipe_ids as $recipe_id ) {
-				
-				$ingredients = get_post_meta( $recipe_id, '_recipe_ingredients', true );
-				
-				if ( $ingredients ) {
-					
-					$order = 0;
-					
-					foreach ( $ingredients as $ingredient ) {
-						
-						$item_id = $items_api->add_item( $recipe_id, 'ingredient', $order );
-						
-						if ( isset( $ingredient['amt'] ) ) {
-							
-							$wpdb->insert(
-								$wpdb->prefix . 'simmer_recipe_itemmeta',
-								array(
-									'recipe_item_id' => $item_id,
-									'meta_key'       => 'amount',
-									'meta_value'     => $ingredient['amt'],
-								),
-								array(
-									'%d',
-									'%s',
-									'%s',
-								)
-							);
-						}
-						
-						if ( isset( $ingredient['unit'] ) ) {
-							
-							$wpdb->insert(
-								$wpdb->prefix . 'simmer_recipe_itemmeta',
-								array(
-									'recipe_item_id' => $item_id,
-									'meta_key'       => 'unit',
-									'meta_value'     => $ingredient['unit'],
-								),
-								array(
-									'%d',
-									'%s',
-									'%s',
-								)
-							);
-						}
-						
-						if ( isset( $ingredient['desc'] ) ) {
-							
-							$wpdb->insert(
-								$wpdb->prefix . 'simmer_recipe_itemmeta',
-								array(
-									'recipe_item_id' => $item_id,
-									'meta_key'       => 'description',
-									'meta_value'     => $ingredient['desc'],
-								),
-								array(
-									'%d',
-									'%s',
-									'%s',
-								)
-							);
-						}
-						
-						$order++;
-					}
-				}
-				
-				$instructions = get_post_meta( $recipe_id, '_recipe_instructions', true );
-				
-				if ( $instructions ) {
-					
-					$order = 0;
-					
-					foreach ( $instructions as $instruction ) {
-						
-						$item_id = $items_api->add_item( $recipe_id, 'instruction', $order );
-						
-						if ( isset( $instruction['desc'] ) ) {
-							
-							$wpdb->insert(
-								$wpdb->prefix . 'simmer_recipe_itemmeta',
-								array(
-									'recipe_item_id' => $item_id,
-									'meta_key'       => 'description',
-									'meta_value'     => $instruction['desc'],
-								),
-								array(
-									'%d',
-									'%s',
-									'%s',
-								)
-							);
-						}
-						
-						if ( isset( $instruction['heading'] ) && 1 == $instruction['heading'] ) {
-							
-							$wpdb->insert(
-								$wpdb->prefix . 'simmer_recipe_itemmeta',
-								array(
-									'recipe_item_id' => $item_id,
-									'meta_key'       => 'is_heading',
-									'meta_value'     => $instruction['heading'],
-								),
-								array(
-									'%d',
-									'%s',
-									'%d',
-								)
-							);
-						}
-						
-						$order++;
-					}
-				}
-			}
-			
-		}
-	}
-	
 	public static function uninstall() {
 		
 		delete_option( 'simmer_version' );
@@ -303,6 +119,78 @@ final class Simmer_Installer {
 			
 			// Remove the custom DB tables.
 			self::drop_db_tables();
+		}
+	}
+	
+	/**
+	 * Upgrade legacy versions of Simmer.
+	 *
+	 * @since  1.3.0
+	 * @access private
+	 */
+	private static function upgrade() {
+		
+		require( plugin_dir_path( __FILE__ ) . 'class-simmer-upgrade.php' );
+		
+		$upgrader = new Simmer_Upgrade;
+		
+		// Upgrade Simmer from 1.2.X to 1.3.X.
+		if ( get_option( 'simmer_version' ) && version_compare( get_option( 'simmer_version' ), '1.3.0', '<' ) ) {
+			$upgrader->from_1_2();
+		}
+	}
+	
+	/**
+	 * Create the custom database tables.
+	 *
+	 * @since  1.3.0
+	 * @access private
+	 *
+	 * @global $wpdb The WordPress database object.
+	 */
+	private static function create_db_tables() {
+		
+		global $wpdb;
+		
+		$items_table_name     = $wpdb->prefix . 'simmer_recipe_items';
+		$item_meta_table_name = $wpdb->prefix . 'simmer_recipe_itemmeta';
+		
+		$query = '';
+		
+		$charset_collate = $wpdb->get_charset_collate();
+		
+		if ( $items_table_name != $wpdb->get_var( "SHOW TABLES LIKE '$items_table_name'" ) ) {
+			
+			// The recipe items table.
+			$query .= "CREATE TABLE $items_table_name (
+				recipe_item_id bigint(20) NOT NULL auto_increment,
+				recipe_item_type varchar(200) NOT NULL DEFAULT '',
+				recipe_id bigint(20) NOT NULL,
+				recipe_item_order int(11) NOT NULL DEFAULT '0',
+				PRIMARY KEY  (recipe_item_id),
+				KEY recipe_id (recipe_id)
+				) $charset_collate;";
+		}
+		
+		if ( $item_meta_table_name != $wpdb->get_var( "SHOW TABLES LIKE '$item_meta_table_name'" ) ) {
+			
+			// The recipe item meta table.
+			$query .= "CREATE TABLE $item_meta_table_name (
+				meta_id bigint(20) NOT NULL auto_increment,
+				recipe_item_id bigint(20) NOT NULL,
+				meta_key varchar(255) NULL,
+				meta_value longtext NULL,
+				PRIMARY KEY  (meta_id),
+				KEY recipe_item_id (recipe_item_id),
+				KEY meta_key (meta_key)
+				) $charset_collate;";
+		}
+		
+		if ( '' != $query ) {
+			
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			
+			dbDelta( $query );
 		}
 	}
 	
